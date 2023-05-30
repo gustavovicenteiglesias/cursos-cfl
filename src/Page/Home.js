f70e8ceb9303ff9992478de56a86c5a23f76897c
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2023-05-30 16:46:13 -0300

    formulario

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 
-const Home =()=>{
+const Home = () => {
+    const [nombre, setNombre] = useState()
+    const [isvissible, setIsvisible] = useState(true)
+    //cada vez que apreto una tecla se lo agrego a nombre
+    const cambiarNombre = (e) => {
+        setNombre(e.target.value)
 
-    return(
-        <div>
-            <h1>Home</h1>
-            <h1>Hola Mundo</h1>
-        </div>
+    }
+
+    //cuando apreto enviar hago visible atraves de setIsVisible al h1 con el nombre 
+    const guardarDatos = (e) => {
+        e.preventDefault();
+        console.log(nombre)
+        setIsvisible(false)
+    }
+
+    return (
+        <>
+            <form onSubmit={(e) => guardarDatos(e)}>
+                <label >Nombre</label>
+                <input name="nombre" onChange={(e) => cambiarNombre(e)}></input>
+                <button type="submit">Enviar</button>
+            </form>
+            {/* imprimir  nombre*/}
+            <div hidden={isvissible}>
+                <h1>Tu nombre es {nombre}</h1>
+            </div>
+        </>
+
     )
 }
 
-export default Home ;
+export default Home;
